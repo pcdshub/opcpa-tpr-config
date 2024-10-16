cef39cbea0bdeb036b1f0b786518762fee1262a4
slactjohnson <tjohnson@slac.stanford.edu>
2024-10-16 13:47:20 -0700

    Initial version of complete RBV panels, based on original implementation.

--- opcpa_tpr_config/widgets.py
+++ opcpa_tpr_config/widgets.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import logging
 from os import path
 
+import happi
 import yaml
 from pydm import Display
 from pydm import widgets as pydm_widgets
@@ -54,6 +55,7 @@ class UserConfigDisplay(Display):
     # Expert mode widgets
     expert_checkbox: QtWidgets.QCheckBox
     xpm_table: pydm_widgets.PyDMNTTable
+    tpr_frame: QtWidgets.QFrame
     rbv_frame: QtWidgets.QFrame
 
     def __init__(
@@ -70,6 +72,8 @@ class UserConfigDisplay(Display):
 
         self.read_config(config)
 
+        self._db = happi.Client(path=self._config['main']['laser_database'])
+
         if self._config is None:
             raise ValueError(f"Could not read config file {config}")
 
@@ -81,7 +85,7 @@ class UserConfigDisplay(Display):
 
         self.screen_title.setText(self._config['main']['title'])
 
-        self.update_pvs()
+        self.update_user_pvs()
 
         self._base_rates = make_base_rates(carbide_factors)
 
@@ -89,7 +93,7 @@ class UserConfigDisplay(Display):
         self.update_goose_rates()
         self.update_goose_arrival()
 
-        self.configure_rbv_frame()
+        self.configure_rbv_frames()
 
         self.update_expert_vis()
 
@@ -124,20 +128,145 @@ class UserConfigDisplay(Display):
         box status.
         """
         self.xpm_table.setVisible(self.expert_mode)
+        self.tpr_frame.setVisible(self.expert_mode)
         self.rbv_frame.setVisible(self.expert_mode)
 
-    def configure_rbv_frame(self):
+    def configure_rbv_frames(self):
         """
         Add a layout and widgets to the rbv frame based on the laser system
         happi database.
         """
-        layout = QtWidgets.QGridLayout()
-        tst_widget = QtWidgets.QLabel()
-        tst_widget.setText("Test")
-        layout.addWidget(tst_widget, 0, 0)
-        self.rbv_frame.setLayout(layout)
+        tpr_layout = QtWidgets.QGridLayout()
+        tpr_layout = self.setup_tpr_rbvs(self._config, self._db, tpr_layout)
+        self.tpr_frame.setLayout(tpr_layout)
 
-    def update_pvs(self):
+        rbv_layout = QtWidgets.QGridLayout()
+        rbv_layout = self.setup_signal_rbvs(self._config, self._db, rbv_layout)
+        self.rbv_frame.setLayout(rbv_layout)
+
+    def setup_tpr_rbvs(self, las_conf, las_db, grid):
+        """
+        Setup RBV widgets for TPR triggers associated with the laser system.
+
+        Arguments
+        ---------
+        las_conf: The key name of the laser to be used.
+        las_db: The file name of the laser happi db.json file
+        grid: The QGridLayout widget to add widgets to
+        """
+        # Setup column headers
+        desc = pydm_widgets.PyDMLabel()
+        desc.setText("Trigger")
+        grid.addWidget(desc, 0, 0)
+
+        reprate = pydm_widgets.PyDMLabel()
+        reprate.setText("Rep. Rate")
+        grid.addWidget(reprate, 0, 1)
+
+        ratemode = pydm_widgets.PyDMLabel()
+        ratemode.setText("Rate Mode")
+        grid.addWidget(ratemode, 0, 2)
+
+        eventcode = pydm_widgets.PyDMLabel()
+        eventcode.setText("Event Code")
+        grid.addWidget(eventcode, 0, 3)
+
+        width = pydm_widgets.PyDMLabel()
+        width.setText("Width (ns)")
+        grid.addWidget(width, 0, 4)
+
+        delay = pydm_widgets.PyDMLabel()
+        delay.setText("Delay (ns)")
+        grid.addWidget(delay, 0, 5)
+
+        op = pydm_widgets.PyDMLabel()
+        op.setText("Logic")
+        grid.addWidget(op, 0, 6)
+
+        enabled = pydm_widgets.PyDMLabel()
+        enabled.setText("Status")
+        grid.addWidget(enabled, 0, 7)
+
+        # Setup TPR PV RBVs
+        tpr_trigs = las_db.search(device_class='pcdsdevices.tpr.TprTrigger')
+        ntrig = 1
+        for trig in tpr_trigs:
+            if trig.metadata['active']:
+                name = trig.metadata['name']
+                if name in las_conf['main']['devices'].keys():
+                    trig_conf = las_conf['main']['devices'][name]
+                    rbvs = trig_conf['rbvs']
+                    for nrbv, rbv in enumerate(rbvs):
+                        child = self.configure_rbv_widget(trig, rbv)
+                        grid.addWidget(child, ntrig, nrbv)
+                    ntrig += 1
+
+        return grid
+
+    def setup_signal_rbvs(self, las_conf, las_db, grid):
+        """
+        Setup RBV widgets for TPR triggers associated with the laser system.
+
+        Arguments
+        ---------
+        las_conf: The key name of the laser to be used.
+        las_db: The file name of the laser happi db.json file
+        grid: The QGridLayout widget to add widgets to
+        """
+        # Setup column headers
+        desc = pydm_widgets.PyDMLabel()
+        desc.setText("Signal")
+        grid.addWidget(desc, 0, 0)
+
+        val = pydm_widgets.PyDMLabel()
+        val.setText("Value")
+        grid.addWidget(val, 0, 1)
+
+        # Setup Signal PV RBVs
+        signals = las_db.search(device_class='ophyd.signal.EpicsSignal')
+        nsig = 1
+        for signal in signals:
+            if signal.metadata['active']:
+                name = signal.metadata['name']
+                if name in las_conf['main']['devices'].keys():
+                    sig_conf = las_conf['main']['devices'][name]
+                    rbvs = sig_conf['rbvs']
+                    for nrbv, rbv in enumerate(rbvs):
+                        child = self.configure_rbv_widget(signal, rbv)
+                        grid.addWidget(child, nsig, nrbv)
+                    nsig += 1
+
+        return grid
+
+    def configure_rbv_widget(self, dev, rbv):
+        """
+        Setup a ophyd device RBV widget.
+
+        Arguments
+        ---------
+        dev: happi Client search result
+        rbv: The device class signal to create an RBV widget for
+             *Note: in the case of EpicsSignals, we use a "val" to indicate
+             that we want to use the base PV of the signal.
+
+        returns:
+            PyDMLabel
+        """
+        child = pydm_widgets.PyDMLabel()
+        device = dev.get()
+        if rbv == 'name':
+            child.setText(getattr(device, 'name'))
+            return child
+        elif rbv == 'val':  # EpicsSignals need pvname, use "val" in config
+            pvname = getattr(device, "pvname")
+        else:
+            pvname = getattr(device, f"{rbv}.pvname")
+        channel = f"ca://{pvname}"
+        child.set_channel(channel)
+
+        return child
+
+    def update_user_pvs(self):
         """
         Modify RBV widgets to use the PV(s) specified in the config file.
         """
