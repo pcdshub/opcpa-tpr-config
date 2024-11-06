bad397727f2ec8d78fa71f313d3bdb8bca26c3b9
slactjohnson <tjohnson@slac.stanford.edu>
2024-11-06 07:38:36 -0800

    Clean up launcher scripts

--- neh_bay3_launcher.sh
+++ neh_bay3_launcher.sh
@@ -4,4 +4,4 @@ launcher_dir="$(dirname ${launcher})"
 
 cd ${launcher_dir}
 
-./launcher.sh opcpa_tpr_config/neh_bay3_config.yaml 2>/dev/null &
+./launcher.sh opcpa_tpr_config/new_neh_bay3_config.yaml
--- new_neh_bay2_launcher.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-launcher="$(realpath $0)"
-launcher_dir="$(dirname ${launcher})"
-
-cd ${launcher_dir}
-
-./new_launcher.sh opcpa_tpr_config/new_neh_bay2_config.yaml
